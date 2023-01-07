ac9e47e1ef3a10938bf42e96c4d43748b1853cec
kingjamesegunn <james@steer.global>
2023-01-07 18:26:04 +0100

    Changed interface

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,39 +1,43 @@
-import Pics from '../assets/mypics.png';
-import PicsSmall from '../assets/picsl.png';
-import '../styles/Pages/About.css';
+import Pics from "../assets/mypics.png";
+import PicsSmall from "../assets/picsl.png";
+import "../styles/Pages/About.css";
 
 function About() {
 	return (
-		<div className='about '>
-			<div className='row'>
-				<div className='about__details col-lg-8 col-md-7 col-xs-12'>
-					<div className='about__title'>
+		<div className="about ">
+			<div className="row">
+				<div className="about__details col-lg-8 col-md-7 col-xs-12">
+					<div className="about__title">
 						<h3>About me</h3>
-						<div className='about__staight'></div>
+						<div className="about__staight"></div>
 					</div>
-					<p className='about__p'>
-						I’m James Oluwasegun OKUNOLA. My passion for web development started
-						far back in 2017 when I just finished my secondary school. I was
-						eager to learn anything that will make me unique. This lead me to
-						start HTML & CSS course on W3schools.
+					<p className="about__p">
+						I’m James Oluwasegun OKUNOLA. I am a highly skilled React developer
+						and UI/UX designer with a strong background in developing React
+						applications (web and mobile) and creating elegant Figma designs. I
+						am a fast learner who is always up-to-date on the latest
+						technologies, and I have a passion for building strong communication
+						with my clients.
 						<br />
 					</p>
-					<p className='about__p'>
-						Today, I’ve been able to work with a startup, individuals and
-						companies. My passion today is to work for a company and promote
-						their products and I am willing to learn from other developers.
+					<p className="about__p">
+						As a team player, I work well with others and am always open to
+						collaborating to achieve outstanding results in web app development
+						and user interface design. With my skills and experience, I am
+						confident in my ability to deliver high-quality projects that meet
+						my client's needs and exceed their expectations.
 						<br />
 					</p>
-					<p className='about__p'>
-						I belive that, “Code will save me, and design will make my life
-						neat”.
+					<p className="about__p">
+						" I'm not a great programmer, I'm just a good programmer with each
+						great habits."
 						<br />
 					</p>
-					<h1 className='about__motto'>I Code, I design</h1>
+					<h1 className="about__motto">I Code, I design</h1>
 				</div>
-				<div className='about_image col-lg-4 col-md-5 col-xs-12'>
-					<img src={Pics} alt='Me' className='image__large' />
-					<img src={PicsSmall} alt='Me' className='image__small' />
+				<div className="about_image col-lg-4 col-md-5 col-xs-12">
+					<img src={Pics} alt="Me" className="image__large" />
+					<img src={PicsSmall} alt="Me" className="image__small" />
 				</div>
 			</div>
 		</div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,12 @@
-import MenuIcon from '../assets/menu.svg';
-import '../styles/components/nav.css';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import GitHubIcon from '../assets/git.svg';
-import LinkIcon from '../assets/link.svg';
-import TwitterIcon from '../assets/twi.svg';
-import WhatsappIcon from '../assets/whatsapp.svg';
-import InstagrmIcon from '../assets/i.svg';
+import MenuIcon from "../assets/menu.svg";
+import "../styles/components/nav.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import GitHubIcon from "../assets/git.svg";
+import LinkIcon from "../assets/link.svg";
+import TwitterIcon from "../assets/twi.svg";
+import WhatsappIcon from "../assets/whatsapp.svg";
+import InstagrmIcon from "../assets/i.svg";
 
 function Nav() {
 	const [menu, setMenu] = useState(false);
@@ -18,87 +18,100 @@ function Nav() {
 		setMenu(true);
 	};
 	return (
-		<div className='nav'>
-			<div className='nav_main'>
-				<div className='logo'>Kings</div>
-				<div className='nav__menu'>
-					<img src={MenuIcon} alt='Menu' onClick={handleMenuClick} />
+		<div className="nav">
+			<div className="nav_main">
+				<div className="logo">Kings</div>
+				<div className="nav__menu">
+					<img src={MenuIcon} alt="Menu" onClick={handleMenuClick} />
 				</div>
 			</div>
 
 			{!menu ? (
-				<div style={{height: "100vh"}}>
-					<ul className='nav_ul'>
-						<li className='nav_links'>
-							<Link to='/' onClick={handleLinkClick}>
-								Home
+				<div style={{ height: "100vh", position: "relative" }}>
+					<ul className="nav_ul">
+						<li className="nav_links">
+							<Link to="/" onClick={handleLinkClick}>
+								<span className="links__num">01</span>
+								<span>Home</span>
 							</Link>
 						</li>
-						<li className='nav_links'>
-							<Link to='/about' onClick={handleLinkClick}>
+						<li className="nav_links">
+							<Link to="/about" onClick={handleLinkClick}>
+								<span className="links__num">02</span>
 								About
 							</Link>
 						</li>
-						<li className='nav_links'>
-							<Link to='/projects' onClick={handleLinkClick}>
+						<li className="nav_links">
+							<Link to="/projects" onClick={handleLinkClick}>
+								<span className="links__num">03</span>
 								Projects
 							</Link>
 						</li>
-						<li className='nav_links'>
-							<Link to='/contact' onClick={handleLinkClick}>
+						<li className="nav_links">
+							<Link to="/contact" onClick={handleLinkClick}>
+								<span className="links__num">04</span>
 								Message me
 							</Link>
 						</li>
 					</ul>
 
-					<div className='nav__socials'>
-						<ul className='socials'>
-							<li className='socials__link'>
-								<a
-									href='https://github.com/kingjamesegun'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<img src={GitHubIcon} alt='Github Link' />
-								</a>
-							</li>
-							<li className='socials__link'>
-								<a
-									href='https://wa.me/message/ICR3VWZS7ACVM1'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<img src={WhatsappIcon} alt='Whatsapp Link' />
-								</a>
-							</li>
-							<li className='socials__link'>
-								<a
-									href='https://twitter.com/kingjamesegun'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<img src={TwitterIcon} alt='Twitter Link' />
-								</a>
-							</li>
-							<li className='socials__link'>
-								<a
-									href='https://www.instagram.com/kingjamesegun/'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<img src={InstagrmIcon} alt='IG ink' />
-								</a>
-							</li>
-							<li className='socials__link'>
-								<a
-									href='https://www.linkedin.com/in/james-okunola-65959519a/'
-									target='_blank'
-									rel='noopener noreferrer'
-								>
-									<img src={LinkIcon} alt='Linkdin Link' />
-								</a>
-							</li>
-						</ul>
+					<div className="nav__btm">
+						<div className="btm__copyright">
+							COPYRIGHT 2023 - <span className="yellow">247SPACE</span> NIGERIA
+						</div>
+						<div className="nav__socials">
+							<ul className="socials">
+								<li className="socials__link">
+									<a
+										href="https://github.com/kingjamesegun"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img src={GitHubIcon} alt="Github Link" />
+									</a>
+								</li>
+								<li className="socials__link">
+									<a
+										href="https://wa.me/message/ICR3VWZS7ACVM1"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img src={WhatsappIcon} alt="Whatsapp Link" />
+									</a>
+								</li>
+								<li className="socials__link">
+									<a
+										href="https://twitter.com/kingjamesegun"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img src={TwitterIcon} alt="Twitter Link" />
+									</a>
+								</li>
+								<li className="socials__link">
+									<a
+										href="https://www.instagram.com/kingjamesegun/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img src={InstagrmIcon} alt="IG ink" />
+									</a>
+								</li>
+								<li className="socials__link">
+									<a
+										href="https://www.linkedin.com/in/james-okunola-65959519a/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<img src={LinkIcon} alt="Linkdin Link" />
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="btm__brand">
+							Website made is made by{" "}
+							<span className="yellow">247space Agency</span>
+						</div>
 					</div>
 				</div>
 			) : null}
